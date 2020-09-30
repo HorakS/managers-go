47e8b339a2898a6f9a0c517fcdf11279addd2855
Stefan Horak <horak.ste@gmail.com>
2020-09-30 16:36:57 +0200

    Read players from json file

--- managers.go
+++ managers.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
-type Pdata struct {
-	Name string
+type Players struct {
+	Players []Player `json:"players"`
 }
 
-func getPdata() (pData Pdata, err error) {
+type Player struct {
+	KickerName string `json:"kickerName"`
+	KickerTeam string `json:"kickerTeam"`
+}
+
+func getPdata() (players Players, err error) {
 	skip := false
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.kicker.de"),
@@ -36,9 +45,28 @@ func getPdata() (pData Pdata, err error) {
 
 	c.Visit("https://www.kicker.de/timo-werner/spieler/bundesliga/2019-20/rb-leipzig")
 	c.Wait()
-	return pData, nil
+	return players, nil
 }
 
 func main() {
-	getPdata()
+	// TODO: pass year/season as flag
+	playersFile := flag.String("players", "players.json", "Json file with all players to be scanned")
+	flag.Parse()
+	fmt.Println(*playersFile)
+
+	jsonFile, err := os.Open(*playersFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var players Players
+	json.Unmarshal(byteValue, &players)
+
+	for i := 0; i < len(players.Players); i++ {
+		fmt.Println("Player name:", players.Players[i].KickerName)
+		fmt.Println("Player team:", players.Players[i].KickerTeam)
+	}
 }

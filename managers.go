5fb61b3d378f6d5c8c0b33449692c12807c28062
Stefan Horak <horak.ste@gmail.com>
2020-10-07 12:57:28 +0200

    Track conceded goals

--- managers.go
+++ managers.go
@@ -22,13 +22,14 @@ type Match struct {
 }
 
 type Pdata struct {
-	Match    Match   `json:"match"`
-	Grade    float64 `json:"grade"`
-	Scp      int     `json:"scp"`
-	Playtime int     `json:"playtime"`
-	SubIn    int     `json:"subIn"`
-	SubOut   int     `json:"subOut"`
-	Top11    bool    `json:"top11"`
+	Match         Match   `json:"match"`
+	Grade         float64 `json:"grade"`
+	Scp           int     `json:"scp"`
+	Playtime      int     `json:"playtime"`
+	SubIn         int     `json:"subIn"`
+	SubOut        int     `json:"subOut"`
+	Top11         bool    `json:"top11"`
+	ConcededGoals float64 `json:"concededGoals"`
 }
 
 type Player struct {
@@ -42,10 +43,11 @@ type Player struct {
 }
 
 type Average struct {
-	Grade    float64 `json:"grade"`
-	Scp      int     `json:"scp"`
-	Playtime int     `json:"playtime"`
-	Top11    int     `json:"top11"`
+	Grade         float64 `json:"grade"`
+	Scp           int     `json:"scp"`
+	Playtime      int     `json:"playtime"`
+	Top11         int     `json:"top11"`
+	ConcededGoals float64 `json:"concededGoals"`
 }
 
 func (m Match) String() string {
@@ -78,23 +80,62 @@ func getPdata(players []Player) (err error) {
 		bundesliga := false
 
 		e.ForEachWithBreak("tr", func(_ int, row *colly.HTMLElement) bool {
-			match := row.ChildText("td:nth-child(1)")
-			if match == "Bundesliga" {
+			category := row.ChildText("td:nth-child(1)")
+			if category == "Bundesliga" {
 				bundesliga = true
 				return true
 			}
 
 			noplay := row.ChildText("td.kick__vita__statistic--table-second_noplay") == "ohne Einsatz im Kader"
-			if !bundesliga || match == "" || noplay {
+			if !bundesliga || category == "" || noplay {
 				return true
 			}
 
-			if !strings.Contains(match, "Spieltag") {
+			if !strings.Contains(category, "Spieltag") {
 				return false
 			}
 
-			matchday, data := parsePdata(row)
-			player.Matches[matchday] = data
+			var data = new(Pdata)
+			var match = new(Match)
+
+			matchInfo := row.ChildText("div.kick__vita__statistic--table-second_dateinfo")
+			matchDay, _ := strconv.Atoi(strings.Split(matchInfo, ".")[0])
+
+			teams := row.ChildAttrs("a.kick__v100-gameCell__team", "href")
+			match.HomeTeam = strings.Split(teams[0], "/")[1]
+			match.GuestTeam = strings.Split(teams[1], "/")[1]
+
+			scores := row.ChildTexts("div.kick__v100-scoreBoard__scoreHolder__score")
+			match.EndScore = scores[0] + ":" + scores[1]
+			match.HalftimeScore = scores[2] + ":" + scores[3]
+
+			var err error
+			data.Grade, _ = strconv.ParseFloat(strings.ReplaceAll(row.ChildText("td:nth-child(2)"), ",", "."), 64)
+			data.Scp, _ = strconv.Atoi(row.ChildText("td:nth-child(6)"))
+			data.SubIn, _ = strconv.Atoi(strings.ReplaceAll(row.ChildText("td:nth-child(7)"), ".", ""))
+			data.SubOut, err = strconv.Atoi(strings.ReplaceAll(row.ChildText("td:nth-child(8)"), ".", ""))
+			if err != nil {
+				data.SubOut = 90
+			}
+			data.Playtime = data.SubOut - data.SubIn
+			data.Match = *match
+			data.Top11 = false // default Top11 is false, may get changed to true later
+
+			if player.KickerTeam == match.HomeTeam {
+				goals, _ := strconv.Atoi(strings.Split(match.EndScore, ":")[1])
+				factor := float64(data.Playtime) / 90.0
+				data.ConcededGoals = float64(goals) * factor
+			} else if player.KickerTeam == match.GuestTeam {
+				goals, _ := strconv.Atoi(strings.Split(match.EndScore, ":")[0])
+				factor := float64(data.Playtime) / 90.0
+				data.ConcededGoals = float64(goals) * factor
+			} else {
+				// If a player changed teams during the season we cannot know
+				// which team he previously played on. Just use 99 as dummy value
+				data.ConcededGoals = 99
+			}
+
+			player.Matches[matchDay] = data
 			return true
 		})
 	})
@@ -123,6 +164,8 @@ func getAverageData(data map[int]*Pdata) (avg *Average, err error) {
 	playtime := 0
 	grade := 0.0
 	top11 := 0
+	goals := 0.0
+	unknownTeamMatches := 0
 	for _, match := range data {
 		scp += match.Scp
 		playtime += match.Playtime
@@ -132,6 +175,11 @@ func getAverageData(data map[int]*Pdata) (avg *Average, err error) {
 		}
 		if match.Top11 {
 			top11 += 1
+		}
+		if match.ConcededGoals != 99 {
+			goals += match.ConcededGoals
+		} else {
+			unknownTeamMatches += 1
 		}
 	}
 	if gradedMatches == 0.0 {
@@ -143,6 +191,7 @@ func getAverageData(data map[int]*Pdata) (avg *Average, err error) {
 	avg.Scp = scp / matches
 	avg.Playtime = playtime / matches
 	avg.Top11 = top11
+	avg.ConcededGoals = goals / float64(matches-unknownTeamMatches)
 	return avg, nil
 }
 
@@ -182,36 +231,6 @@ func getTop11Data(players []Player) (err error) {
 	}
 
 	return nil
-}
-
-func parsePdata(row *colly.HTMLElement) (matchDay int, pData *Pdata) {
-	data := new(Pdata)
-	match := new(Match)
-
-	matchInfo := row.ChildText("div.kick__vita__statistic--table-second_dateinfo")
-	matchDay, _ = strconv.Atoi(strings.Split(matchInfo, ".")[0])
-	teams := row.ChildTexts("div.kick__v100-gameCell__team__name")
-	// TODO: Instead extract kicker team names from hrefs?
-	match.HomeTeam = teams[0]
-	match.GuestTeam = teams[1]
-
-	scores := row.ChildTexts("div.kick__v100-scoreBoard__scoreHolder__score")
-	match.EndScore = scores[0] + ":" + scores[1]
-	match.HalftimeScore = scores[2] + ":" + scores[3]
-
-	var err error
-	data.Grade, _ = strconv.ParseFloat(strings.ReplaceAll(row.ChildText("td:nth-child(2)"), ",", "."), 64)
-	data.Scp, _ = strconv.Atoi(row.ChildText("td:nth-child(6)"))
-	data.SubIn, _ = strconv.Atoi(strings.ReplaceAll(row.ChildText("td:nth-child(7)"), ".", ""))
-	data.SubOut, err = strconv.Atoi(strings.ReplaceAll(row.ChildText("td:nth-child(8)"), ".", ""))
-	if err != nil {
-		data.SubOut = 90
-	}
-	data.Playtime = data.SubOut - data.SubIn
-	data.Match = *match
-	data.Top11 = false // default Top11 is false, may get changed to true later
-
-	return matchDay, data
 }
 
 func writeCsv(players []Player) (err error) {
